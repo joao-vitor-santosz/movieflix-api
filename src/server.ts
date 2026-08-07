c4d6a5fcc3ca49a0ff6218833a37f6dd9acc838f
João Vítor <joaovns0812@gmail.com>
2026-08-06 23:04:38 -0400

    feat: rota de POST foi adicionada, juntamente, com tratamento de erro

--- src/server.ts
+++ src/server.ts
@@ -8,20 +8,43 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const app = express();
 const port = 3000;
 const prisma = new PrismaClient({
-  adapter,
+    adapter,
 });
+
+app.use(express.json());
 
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
-            title: "asc",
+            title: 'asc',
         },
         include: {
             genres: true,
             languages: true,
-        }
+        },
     });
     res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (err) {
+        res.status(500).send({ message: 'Falha ao cadatrar o filme' });
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
